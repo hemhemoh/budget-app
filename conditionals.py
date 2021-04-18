4da36a7cf26afec90541c3c32912183bfb84ff5a
hemhemoh <70777991+hemhemoh@users.noreply.github.com>
2021-04-18 04:41:25 -0700

    Add files via upload

---
+++ conditionals.py
@@ -0,0 +1,7 @@
+number = 20
+#if number > 20:
+    #print("big girl!!")
+#else:
+    #print("GOAN meet your mummy")
+
+print("Welcome" if number > 18 else "Goan meet your mummy you {} year old".format(number))
